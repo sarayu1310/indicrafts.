c09139aea6d533b31d410195e476e7bf1529c0d6
sarayu1310 <sarayugosika2004@gmail.com>
2025-11-04 16:19:41 +0530

    changed url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-  origin: "https://indicrafts.netlify.app/",
+  origin: "https://indicrafts.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
